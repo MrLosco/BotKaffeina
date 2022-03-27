190e88eb373c5fdf558b99d0344d176a8980e88d
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-03-27 09:56:00 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -276,7 +276,7 @@ function oraAttuale() {
     var minutes = new Date().getMinutes();
 
     var canale = client.channels.cache.get("949783943304073299")
-    if(hour == 09 && minutes == 48) {
+    if (hour == 10 minutes == 0) {
         canale.send("Buon giorno gente @everyone")
     }
 }
