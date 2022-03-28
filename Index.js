078c80a57c98dc41594df4fc9e994dac2adbf6c3
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-03-28 14:11:58 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -114,6 +114,7 @@ client.on("messageCreate", message => {
                 songsList += `${i + 1}. **${queue.songs[i].name.length <= 100 ? queue.songs[i].name : `${queue.songs[i].name.slice(0, 100)}...`}** - ${queue.songs[i].formattedDuration}\r`
             }
         }
+    }    
 
         let embed = new Discord.MessageEmbed()
             .addField("Queue", songsList)
@@ -185,8 +186,7 @@ client.on("messageCreate", message => {
                     msg.edit({ embeds: [embed], components: [row] })
                 })
             })
-    }
-
+    
     if (message.content == ".skip") {
         const voiceChannel = message.member.voice.channel
         if (!voiceChannel) {
@@ -249,33 +249,8 @@ client.on("messageCreate", message => {
 
         message.channel.send("Queue stoppata")
     }
+})
     
-    if (message.content === '.tempo') {
-        axios
-            .get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=${apitoken}`
-            )
-            .then(response => {
-                let apiData = response;
-                let currentTemp = Math.ceil(apiData.data.main.temp)
-                let maxTemp = apiData.data.main.temp_max;
-                let minTemp = apiData.data.main.temp_min;
-                let humidity = apiData.data.main.humidity;
-                let wind = apiData.data.wind.speed;
-                let author = message.author.username
-                let profile = message.author.displayAvatarURL
-                let icon = apiData.data.weather[0].icon
-                let cityName = args
-                let country = apiData.data.sys.country
-                let pressure = apiData.data.main.pressure;
-                let cloudness = apiData.data.weather[0].description;
-                message.channel.send(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country));
-            }).catch(err => {
-                message.reply(`Enter a vailid city name`)
-            })
-
-
-
 distube.on("addSong", (queue, song) => {
     let embed = new Discord.MessageEmbed()
         .setTitle("Traccia aggiunta")
@@ -315,33 +290,4 @@ function oraAttuale() {
         canale.send("Buonanotte ragazzuoli @everyone")
     }
 }
-setInterval(oraAttuale, 1000 * 60);
-
-const exampleEmbed = (
-	temp,
-	maxTemp,
-	minTemp,
-	pressure,
-	humidity,
-	wind,
-	cloudness,
-	icon,
-	author,
-	profile,
-	cityName,
-	country
-) =>
-	new Discord.RichEmbed()
-		.setColor('#0099ff')
-		.setAuthor(`Hello, ${author}`, profile)
-		.setTitle(`There is ${temp}\u00B0 C in ${cityName}, ${country}`)
-		.addField(`Maximum Temperature:`, `${maxTemp}\u00B0 C`, true)
-		.addField(`Minimum Temperature:`, `${minTemp}\u00B0 C`, true)
-		.addField(`Humidity:`, `${humidity} %`, true)
-		.addField(`Wind Speed:`, `${wind} m/s`, true)
-		.addField(`Pressure:`, `${pressure} hpa`, true)
-		.addField(`Cloudiness:`, `${cloudness}`, true)
-		.setThumbnail(`http://openweathermap.org/img/w/${icon}.png`)
-		.setFooter('Made With 💖 by @MrLosco');
-
-        
+setInterval(oraAttuale, 1000 * 60)
