c5dea2ec1888f0d20ef542a0e8f95634678fabcf
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-03-28 14:31:00 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const axios = require('axios');
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] 
 })
