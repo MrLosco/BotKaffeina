df38fb893a6766f93c2009cecf260eee6972d829
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-03-29 22:37:31 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,45 +1,9 @@
-const Discord = require('discord.js');
-global.client = new Discord.Client({
-    intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] 
-})
+const { Client, Intents } = require('discord.js');
 
-client.login("OTU2MjkwMjY5ODMyMTY3NTA0.YjuEtw.pBw-7eIQjIldsRvCMijVpCTw1VI")
+const client = new Client({ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] });
 
-client.on("ready", () => {
-    console.log("ONLINE");
-})
+client.once('ready', () => {
+    console.log('Bot online');
+});
 
-const fs = require("fs");
-
-client.commands = new Discord.Collection();
-
-const commandsFile = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-for(const file of commandsFile) {
-    var command = require('./commands/${file}');
-    client.commands.set(command.name, command);
-}
-
-const { DisTube } = require("distube")
-
-const { SpotifyPlugin} = require("@distube/spotify")
-const { SoundCloudPlugin } = require("@distube/soundcloud")
-
-const distube = new DisTube(client, {
-    youtubeDL: false,
-    plugins: [new SpotifyPlugin(), new SoundCloudPlugin()],
-    leaveOnEmpty: true,
-    leaveOnStop: true
-})
-
-client.on("messageCreate", message => {
-    const prefix = "."
-
-    if(!message.content.startWith(prefix)) return
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLoverCase();
-
-    if(!client.commands.has(command)) return
-
-    client.command.get(command).execute(messageCreate, args);
-})
+client.login("OTU2MjkwMjY5ODMyMTY3NTA0.YjuEtw.pBw-7eIQjIldsRvCMijVpCTw1VI");
