26e9d978668201a103442ac49ec6865b75cab029
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-03-30 09:52:11 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -249,9 +249,9 @@ client.on("messageCreate", message => {
         message.channel.send("Queue stoppata")
     }
 
-    if (message.content == ".repeat") {
+    if (message.content == ".repeats") {
         const args = message.content.slice(".repeat".length).trim().split(/ +/g);
-        let mode = distube.setRepeatMode(message, parseInt(args[0]));
+        let mode = distube.setRepeatMode(message, parseInt(args[1]));
         mode = mode ? mode == 2 ? "Repeat queue" : "Repeat song" : "Off";
         message.channel.send("Set repeat mode to `" + mode + "`");
     }
