3959777857fc423a8b8650b2cd4aafb08c68b855
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-03-30 20:27:55 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -286,14 +286,14 @@ client.on("messageCreate", message => {
     if (message.content == "!lastvideo") {
         const channelId = 'UCLrgUeP56dUPUwp4vCy6RIQ' 
         ytch.getChannelVideos(channelId, "newest").then((response) => {
-            var embed = new Discord.MessageEmbed()
+            var embedLastVideo = new Discord.MessageEmbed()
                 .setTitle(response.items[0].title)
                 .setURL("https://www.youtube.com/watch?v=" + response.items[0].videoId)
                 .setThumbnail(response.items[0].videoThumbnails[3].url)
                 .addField("Views", response.items[0].viewCount.toString(), true)
                 .addField("Duration", response.items[0].durationText, true)
                 .addField("Published", response.items[0].publishedText, true)
-            message.channel.send({embeds: [embeds] })
+            message.channel.send({embeds: [embedLastVideo] })
         })
     }
 })
