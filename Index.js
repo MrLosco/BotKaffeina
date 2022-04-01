e090a5edd53781ec6d63e1a0d6d17b669a2d1add
MrLosco <102362308+MrLosco@users.noreply.github.com>
2022-04-01 15:13:04 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"] 
 })
 
-client.login(processo.env.token)
+client.login(process.env.token)
 
 client.on("ready", () => {
     console.log("BOT ONLINE by MrLosco");
